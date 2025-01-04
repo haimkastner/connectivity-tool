d4d288b0ed0eab9ab371b364ad89ed8a4013c72b
haimkastner <haimkastner@users.noreply.github.com>
2025-01-04 20:07:07 +0000

    Update to version 1.0.11 [skip-ci]
    Files changed:
    M	setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 import os
 from datetime import datetime
 
-version = '1.0.10'
+version = '1.0.11'
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text(encoding='utf-8')
